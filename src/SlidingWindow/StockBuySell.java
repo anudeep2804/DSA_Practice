02d40c7d90b8f5b7ec3a8563a8a3670cbd1e7974
anudeep2804 <billa.anudeep143@gmail.com>
2023-03-21 17:04:38 -0400

    Commiting changes

--- src/SlidingWindow/StockBuySell.java
+++ src/SlidingWindow/StockBuySell.java
@@ -4,4 +4,26 @@ package SlidingWindow;
 
 public class StockBuySell {
 
+        public static int maxProfit(int[] prices) {
+            int left = 0;
+            int right = 1;
+            int maxProfit = 0;
+            while (right < prices.length) {
+                if (prices[left] < prices[right]) {
+                    maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+                    right++;
+                } else {
+                    left = right;
+                    right++;
+                }
+            }
+            return maxProfit;
+
+        }
+
+
+    public static void main(String[] args) {
+        int input[]= {2,1,4};
+        System.out.println(maxProfit(input));
+    }
 }
