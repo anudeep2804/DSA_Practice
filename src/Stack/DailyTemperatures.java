334b05415302190bb0a90b09d941cbdf6037429f
anudeep2804 <billa.anudeep143@gmail.com>
2023-04-12 23:02:18 -0400

    DailyTemperatures

---
+++ src/Stack/DailyTemperatures.java
@@ -0,0 +1,22 @@
+package Stack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int n = temperatures.length;
+        int [] ans = new int [n];
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                ans[i] = st.peek() - i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
