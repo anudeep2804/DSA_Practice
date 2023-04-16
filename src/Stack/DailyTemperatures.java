b8e801922227360d1c1903e93842d2ad5b196da1
anudeep2804 <billa.anudeep143@gmail.com>
2023-04-15 20:20:58 -0400

    DailyTemperatures updtaed

--- src/Stack/DailyTemperatures.java
+++ src/Stack/DailyTemperatures.java
@@ -1,10 +1,11 @@
 package Stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class DailyTemperatures {
 
-    public int[] dailyTemperatures(int[] temperatures) {
+    public static int[] dailyTemperatures(int[] temperatures) {
         int[] ans = new int[temperatures.length];
         Stack<Integer> stack = new Stack<>();
         for (int currDay = 0; currDay < temperatures.length; currDay++) {
@@ -12,11 +13,16 @@ public class DailyTemperatures {
                     !stack.isEmpty() &&
                             temperatures[currDay] > temperatures[stack.peek()]
             ) {
-                int prevDay = stack.pop();
-                ans[prevDay] = currDay - prevDay;
+                int previousday = stack.pop();
+                ans[previousday] = currDay - previousday;
             }
             stack.add(currDay);
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        int [] ans= dailyTemperatures(new int[]{73,74,75,71,69,72,76,73});
+        System.out.println(Arrays.toString(ans));
     }
 }
