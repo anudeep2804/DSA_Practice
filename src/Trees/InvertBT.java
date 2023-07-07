f61101d833b44878934c43138034bb2d26387f5e
anudeep2804 <billa.anudeep143@gmail.com>
2023-07-07 17:40:38 -0400

    Invert Binary Tree

---
+++ src/Trees/InvertBT.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class InvertBT {
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root==null){
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left= root.right;
+        root.right=temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+
+    }
+}
