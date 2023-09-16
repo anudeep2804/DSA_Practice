e8ab9899f805282214cc99cf39fbff6cddb5196d
anudeep2804 <billa.anudeep143@gmail.com>
2023-09-16 16:41:18 -0400

    Zig Zag Traversal of Binary Tree

---
+++ src/Trees/ZigZagTraversalofBT.java
@@ -0,0 +1,63 @@
+package Trees;
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+public class ZigZagTraversalofBT {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<TreeNode> deque = new ArrayDeque<>();
+
+
+        if(root==null){
+            return result;
+        }
+
+        deque.addFirst(root);
+        int counter=1;
+
+        while(!deque.isEmpty()){
+            int currentqueuesize=deque.size();
+            List<Integer> templist = new ArrayList<>();
+            TreeNode currentnode;
+
+            if(counter %2 !=0){
+                for(int i=0;i<currentqueuesize;i++){
+                    currentnode=deque.removeLast();
+                    templist.add(currentnode.val);
+
+                    if(currentnode.left!=null){
+                        deque.offer(currentnode.left);
+                    }
+
+                    if(currentnode.right!=null){
+                        deque.addFirst(currentnode.right);
+                    }
+                }
+            }
+            else {
+
+                for (int i = 0; i < currentqueuesize; i++) {
+                    currentnode = deque.removeLast();
+                    templist.add(currentnode.val);
+
+                    if (currentnode.right != null) {
+                        deque.addLast(currentnode.right);
+                    }
+
+                    if (currentnode.left != null) {
+                        deque.addLast(currentnode.left);
+                    }
+                }
+
+            }
+            counter++;
+
+            result.add(templist);
+        }
+
+        return result;
+    }
+}
