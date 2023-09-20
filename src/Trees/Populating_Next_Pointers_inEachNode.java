8fac3cd190bbff8e2b98afa5d727cb0e092c4c87
anudeep2804 <billa.anudeep143@gmail.com>
2023-09-19 23:35:45 -0400

    Populating next pointers for each node

---
+++ src/Trees/Populating_Next_Pointers_inEachNode.java
@@ -0,0 +1,67 @@
+package Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
+class Populating_Next_Pointers_inEachNode {
+    public Node connect(Node root) {
+        if(root == null){
+            return null;
+        }
+
+        Queue<Node> queue= new LinkedList<>();
+        queue.offer(root);
+        Node currentnode;
+
+
+        while(!queue.isEmpty()){
+            int currentsize=queue.size();
+            int tempcounter=0;
+
+            for(int i=0;i<currentsize;i++){
+                currentnode=queue.poll();
+
+                if(tempcounter!=currentsize-1){
+                    currentnode.next=queue.peek();
+                }
+
+
+
+
+                if(currentnode.left!=null){
+                    queue.offer(currentnode.left);
+                }
+
+                if(currentnode.right!=null){
+                    queue.offer(currentnode.right);
+                }
+
+                tempcounter++;
+            }
+        }
+
+        return root;
+    }
+}
+
