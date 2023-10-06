7b3adfb058d9762340e713246424b97eb2b683f3
anudeep2804 <billa.anudeep143@gmail.com>
2023-10-06 01:31:49 -0400

    All Subsets of a String

--- src/Recursion/PrintAllSubsets.java
+++ src/Recursion/PrintAllSubsets.java
@@ -28,10 +28,32 @@ public class PrintAllSubsets {
         return processedlist;
     }
 
+    private List<List<Character>> printsubstringsiterative(String s){
+
+        List<List<Character>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(char ch : s.toCharArray()){
+            int n = outer.size();
+            for(int i=0;i<n;i++){
+                List<Character> internal = new ArrayList<>(outer.get(i));
+                internal.add(ch);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+
     public static void main(String[] args) {
         PrintAllSubsets printAllSubsets = new PrintAllSubsets();
         List<String> ans = printAllSubsets.printallSubsets("abc");
-        System.out.println(ans);
+       // System.out.println(ans);
+        List<List<Character>> ans1 = printAllSubsets.printsubstringsiterative("abc");
+
+        for(List<Character> chars : ans1){
+            System.out.println(chars);
+        }
     }
 
 
