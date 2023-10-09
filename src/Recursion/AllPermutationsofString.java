a40bb8f4b6d4ada20485d98c0851f7e2d32d145b
anudeep2804 <billa.anudeep143@gmail.com>
2023-10-08 22:50:42 -0400

    All Permutations of a String in a List

--- src/Recursion/AllPermutationsofString.java
+++ src/Recursion/AllPermutationsofString.java
@@ -2,6 +2,9 @@ package Recursion;
 
 import SlidingWindow.PermutationinString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllPermutationsofString {
 
     public static void PermutationsOfString(String processed, String unprocessed){
@@ -25,7 +28,33 @@ public class AllPermutationsofString {
         }
     }
 
+    public static List<String> PermutationsOfStringList(String processed, String unprocessed){
+
+        if(unprocessed.isEmpty() ){
+           // System.out.println(processed);
+            ArrayList<String> processedStringList = new ArrayList<>();
+            processedStringList.add(processed);
+            return processedStringList;
+        }
+
+        char ch = unprocessed.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+
+        /*
+        Calling the recursion based on length of processed length
+        for example if processed = a and unprocessed =b; the there are two options that is ba and ab
+         */
+        for(int i=0;i<=processed.length();i++){
+
+            String first= processed.substring(0,i);
+            String last = processed.substring(i);
+            ans.addAll(PermutationsOfStringList(first+ch+last , unprocessed.substring(1)));
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        PermutationsOfString("","a");
+        System.out.println(PermutationsOfStringList("","abc"));
     }
 }
