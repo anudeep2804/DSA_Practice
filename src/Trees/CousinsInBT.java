9759f353a77bf0e6b6573da5e2212078c316999a
anudeep2804 <billa.anudeep143@gmail.com>
2023-11-04 09:23:46 -0400

    updated code with efficient sol for cousins of BT

--- src/Trees/CousinsInBT.java
+++ src/Trees/CousinsInBT.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 
     public class CousinsInBT {
 
+        /*
+
         public boolean isCousins(TreeNode root, int x, int y) {
             boolean ans = false;
             Queue<TreeNode> queue = new LinkedList<>();
@@ -55,6 +57,50 @@ import java.util.Queue;
                 }
             }
             return ans;
+        }
+
+         */
+
+        public boolean isCousins(TreeNode root, int x, int y) {
+            if (root == null) return false;
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                boolean isXFound = false;
+                boolean isYFound = false;
+
+                for (int i = 0; i < size; i++) {
+                    TreeNode current = queue.poll();
+
+                    // Check if the current node is x or y and mark them as found.
+                    if (current.val == x) isXFound = true;
+                    if (current.val == y) isYFound = true;
+
+                    // Check if x and y are siblings (have the same parent), if so, return false.
+                    if (current.left != null && current.right != null) {
+                        if ((current.left.val == x && current.right.val == y) ||
+                                (current.left.val == y && current.right.val == x)) {
+                            return false;
+                        }
+                    }
+
+                    // Add the children to the queue for the next level.
+                    if (current.left != null) queue.offer(current.left);
+                    if (current.right != null) queue.offer(current.right);
+                }
+
+                // If both x and y are found at the same level, return true.
+                if (isXFound && isYFound) return true;
+
+                // If one of x or y is found at the current level, return false as they are not at the same level.
+                if (isXFound || isYFound) return false;
+            }
+
+            // If neither x nor y are found, return false.
+            return false;
         }
 
     }
