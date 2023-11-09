fdea8a69984536acfb9b674def22ec8b16f28b92
anudeep2804 <billa.anudeep143@gmail.com>
2023-11-09 18:19:18 -0500

    Lowest Common Ancestor of BST

---
+++ src/Trees/LowestCommonAncestor.java
@@ -0,0 +1,18 @@
+package Trees;
+
+import Trees.TreeNode;
+
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        int curr=root.val;
+
+        if(curr<p.val && curr<q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if(curr>p.val && curr>q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
