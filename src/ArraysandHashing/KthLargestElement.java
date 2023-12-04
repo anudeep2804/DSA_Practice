4e2ddd3ae8e95e130625200af7b2906ad7a79eb4
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-03 19:43:19 -0500

    Kth Largest element in an array

---
+++ src/ArraysandHashing/KthLargestElement.java
@@ -0,0 +1,25 @@
+package ArraysandHashing;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for(int n : nums){
+            priorityQueue.offer(n);
+        }
+
+        int i=0;
+
+        while(i< nums.length-k){
+            priorityQueue.poll();
+            i++;
+        }
+
+        return  priorityQueue.peek();
+    }
+
+}
