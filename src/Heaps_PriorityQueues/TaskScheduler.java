5cf6745a3beb067e3f7675c9f9e3f0cf579e84fd
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-09 12:35:42 -0500

    Task Scheduler

--- src/Heaps_PriorityQueues/TaskScheduler.java
+++ src/Heaps_PriorityQueues/TaskScheduler.java
@@ -7,6 +7,8 @@ import java.util.HashSet;
 
 public class TaskScheduler {
 
+
+
     public int leastInterval(char[] tasks, int n) {
 
         if (tasks == null) {
@@ -37,4 +39,38 @@ public class TaskScheduler {
 
 
     }
+        /*
+
+
+     Heap based solution
+
+    public int leastInterval(char[] tasks, int n) {
+        if (n == 0) return tasks.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int[] arr = new int[26];
+        for (char c : tasks) arr[c - 'A']++;
+        for (int val : arr) if (val > 0) pq.add(val);
+        int time = 0;
+
+        while ((!pq.isEmpty() || !q.isEmpty())) {
+            time++;
+            if (!pq.isEmpty()) {
+                int val = pq.poll();
+                val--;
+                if (val > 0) q.add(new Pair(val, time + n));
+            }
+
+            if (!q.isEmpty() && q.peek().getValue() == time) pq.add(
+                q.poll().getKey()
+            );
+        }
+        return time;
+    }
+
+
+         */
+
+
+
 }
