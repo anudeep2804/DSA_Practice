2e0da1a93d9a2df1e93abc5116e2736ee9f1ecf3
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-09 23:48:50 -0500

    transpose of a matrix

---
+++ src/ArraysandHashing/TransposeMatrix.java
@@ -0,0 +1,25 @@
+package ArraysandHashing;
+
+public class TransposeMatrix {
+
+    public int[][] transpose(int[][] matrix) {
+
+        if(matrix==null){
+            return matrix;
+        }
+
+
+        int[][] result = new int[matrix[0].length][matrix.length];
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+
+        return result;
+
+
+    }
+}
