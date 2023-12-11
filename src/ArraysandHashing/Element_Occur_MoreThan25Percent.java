6caa7d3857aaf5ac637ca19032bdaf9664acf6fb
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-10 19:52:04 -0500

    elements appearing more than 25% of time

---
+++ src/ArraysandHashing/Element_Occur_MoreThan25Percent.java
@@ -0,0 +1,26 @@
+package ArraysandHashing;
+
+public class Element_Occur_MoreThan25Percent {
+
+    public int findSpecialInteger(int[] arr) {
+
+        int count=1;
+
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                count++;
+                if(count>arr.length/4){
+                    return arr[i];
+                }
+            }
+            else{
+                count=1;
+            }
+
+
+        }
+
+        return 1;
+
+    }
+}
