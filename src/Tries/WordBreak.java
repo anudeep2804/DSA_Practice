8afc3831c69ad1c62f3021eb73054c32a9b5c4d0
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-13 18:57:51 -0500

    Word Break Problem Tried using Tries , Recursion ,Bruteforce and Dp

---
+++ src/Tries/WordBreak.java
@@ -0,0 +1,145 @@
+package Tries;
+
+import java.util.List;
+
+public class WordBreak {
+
+
+    public boolean wordBreak(String s , List<String> wordDict){
+        boolean[] table = new boolean[s.length()];
+
+        for(int i=0;i<table.length;i++){
+            for(int y=0;y<=i;y++){
+                if(y==0 || table[y-1]){
+                    if(wordDict.contains(s.substring(y,i+1))){
+                        table[i]=true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return table[table.length-1];
+    }
+
+
+    /*
+    Trie based Approach , gives TLE
+
+
+    private Node root = new Node(); // Initialize the root node
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (wordDict.isEmpty()) return false; // Check if the dictionary is empty
+        ListtoTrie(wordDict);
+        return wordBreakhelper(s);
+    }
+
+    private boolean wordBreakhelper(String s) {
+        int len = s.length();
+        if (len == 0) {
+            return true;
+        }
+
+        for (int i = 1; i <= len; i++) {
+            if (search(s.substring(0, i)) && wordBreakhelper(s.substring(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void ListtoTrie(List<String> wordDict) {
+        for (String str : wordDict) {
+            insert(str);
+        }
+    }
+
+    public boolean search(String word) {
+        Node res = getLast(word);
+        return (res != null && res.isWord);
+    }
+
+    public Node getLast(String word) {
+        Node curr = root;
+        for (char x : word.toCharArray()) {
+            if (curr.children[x - 'a'] == null) {
+                return null;
+            }
+            curr = curr.children[x - 'a'];
+        }
+        return curr;
+    }
+
+    class Node {
+        private boolean isWord;
+        private Node[] children;
+
+        public Node() {
+            this.isWord = false;
+            this.children = new Node[26]; // For 26 lowercase letters
+        }
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (char x : word.toCharArray()) {
+            if (curr.children[x - 'a'] == null) {
+                curr.children[x - 'a'] = new Node();
+            }
+            curr = curr.children[x - 'a'];
+        }
+        curr.isWord = true;
+    }
+  */
+
+
+    /*
+    Recursive Approach , Gives TLE
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+    if (s.isEmpty()) {
+        return true;
+    }
+
+    for (int i = 1; i <= s.length(); i++) {
+        if (wordDict.contains(s.substring(0, i)) && wordBreak(s.substring(i), wordDict)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+    //
+
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            if (wordDict.contains(s.substring(0, i))) {
+                s = s.substring(i);
+                System.out.println(s);
+                if (s.isEmpty()|| wordDict.contains(s)) {
+                    return true;
+                }
+                i = 0; // Reset i to 0 instead of 1
+            }
+        }
+
+        return false;
+    }
+
+ */
+
+
+
+
+
+}
