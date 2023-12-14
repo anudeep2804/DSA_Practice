ebf7c7e67035a7ae8f12f9ef42504e1676953328
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-13 20:14:13 -0500

    Word Break using DP

--- src/Tries/WordBreak.java
+++ src/Tries/WordBreak.java
@@ -1,12 +1,40 @@
 package Tries;
 
+
+
 import java.util.List;
 
 public class WordBreak {
 
+    //  https://leetcode.com/problems/word-break/description/
 
-    public boolean wordBreak(String s , List<String> wordDict){
+
+      //Dp based solution , optimal
+        public boolean wordBreak(String s, List<String> wordDict) {
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[s.length()] = true;
+
+            for(int i = s.length()-1; i >= 0; i--){
+                for(String w: wordDict){
+                    if((i + w.length()) <= s.length() && s.startsWith(w, i)){
+                        dp[i] = dp[i + w.length()];
+                    }
+                    if(dp[i]){
+                        break;
+                    }
+                }
+            }
+            return dp[0];
+        }
+
+
+
+        /* Beats 30%
+
+    public boolean wordBreak( String s , List<String> wordDict){
         boolean[] table = new boolean[s.length()];
+
+
 
         for(int i=0;i<table.length;i++){
             for(int y=0;y<=i;y++){
@@ -21,6 +49,8 @@ public class WordBreak {
 
         return table[table.length-1];
     }
+
+*/
 
 
     /*
