6d244779e4a0ad1c60d13f4895b1b3de58adb611
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-15 14:16:24 -0500

    Destination City

---
+++ src/ArraysandHashing/DestinationCity.java
@@ -0,0 +1,21 @@
+package ArraysandHashing;
+
+import java.util.*;
+
+public class DestinationCity {
+
+    public String destCity(List<List<String>> paths) {
+        Set<String> cities = new HashSet<>();
+        for (List<String> path : paths) {
+            cities.add(path.get(0));
+        }
+
+        for (List<String> path : paths) {
+            String dest = path.get(1);
+            if (!cities.contains(dest)) {
+                return dest;
+            }
+        }
+        return "";
+    }
+}
