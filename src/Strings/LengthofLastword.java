66117c793bd5a8fc9026e7c9f20ec89c9592e543
anudeep2804 <billa.anudeep143@gmail.com>
2023-12-18 23:50:21 -0500

    Length of Last word

---
+++ src/Strings/LengthofLastword.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class LengthofLastword {
+
+    public int lengthOfLastWord(String s) {
+
+        int counter=0;
+
+        char[] array = s.toCharArray();
+
+        int end = array.length-1;
+
+        for(int i=array.length-1  ;i>=0;i--) {
+
+            if (i != end && array[i] == ' ') {
+                return end - i ;
+            }
+
+            else  if (array[i] == ' ') {
+                end--;
+            }
+
+            else{
+                counter++;
+            }
+
+        }
+
+        return counter;
+    }
+}
+
