b82481c325734a53722ec9d181e527ae72b5b299
Anudeep Billa <billa.anudeep143@gmail.com>
2023-12-27 19:19:21 -0500

    Merge Intervals

--- src/ArraysandHashing/MergeIntervals.java
+++ src/ArraysandHashing/MergeIntervals.java
@@ -1,17 +1,39 @@
 package ArraysandHashing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class MergeIntervals {
 
     public static int[][] merge(int[][] intervals) {
 
-        if(intervals.length==1){
-            return null;
+        if (intervals.length <= 1)
+            return intervals;
+
+        // Sort by ascending starting point
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+
+        for (int[] interval : intervals) {
+            if (interval[0] <= newInterval[1])
+                // Overlapping intervals,
+                // update the end if needed
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            else {
+                // Disjoint intervals,
+                // add the new interval to the list
+                newInterval = interval;
+                result.add(newInterval);
+            }
         }
 
-
-        return null;
+        return result.toArray(new int[result.size()][]);
 
 
     }
@@ -19,7 +41,11 @@ public class MergeIntervals {
     public static void main(String[] args) {
 
         int[][] input = {{1,3},{2,6},{8,10},{15,18}};
-        System.out.println(merge(input));
+        int[][] result = merge(input);
+
+        for(int[] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
     }
 
 }
