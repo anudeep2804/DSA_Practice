65495458752b13876d1f78924942104a04456f3b
Anudeep Billa <billa.anudeep143@gmail.com>
2024-01-02 18:54:40 -0500

    Convert an Array Into a 2D Array With Conditions

---
+++ src/ArraysandHashing/ConvertArray_to_2DArray.java
@@ -0,0 +1,46 @@
+package ArraysandHashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertArray_to_2DArray {
+
+    // https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/?envType=daily-question&envId=2024-01-02
+
+    public List<List<Integer>> findMatrix(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> currentlist = new ArrayList<>();
+
+        currentlist.add(nums[0]);
+
+        result.add(currentlist);
+
+        for(int i=1; i<nums.length;i++){
+
+            boolean inserted=false;
+
+            int size = result.size();
+
+            for(int j=0 ; j<size;j++){
+                if(!result.get(i).contains(nums[i])){
+                    result.get(i).add(nums[i]);
+                    inserted=true;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            if(!inserted){
+                List<Integer> templist = new ArrayList<>();
+                templist.add(nums[i]);
+                result.add(templist);
+            }
+        }
+
+        return result;
+
+    }
+}
