681b11f9f3fb2946557c022cd197a0ef679c2326
Anudeep Billa <billa.anudeep143@gmail.com>
2024-03-10 22:18:58 -0400

    Num of Provinces

--- src/Graphs/Number_Of_Provinces.java
+++ src/Graphs/Number_Of_Provinces.java
@@ -1,4 +1,5 @@
 package Graphs;
+
 import java.util.*;
 
 public class Number_Of_Provinces {
@@ -8,19 +9,19 @@ public class Number_Of_Provinces {
         boolean[] visited = new boolean[isConnected.length];
         Queue<Integer> queue = new LinkedList<>();
 
-        int counter =0;
+        int counter = 0;
 
-        for(int i=0; i<isConnected.length; i++){
-            if(visited[i]==false){
+        for (int i = 0; i < isConnected.length; i++) {
+            if (visited[i] == false) {
                 queue.offer(i);
 
-                while(!queue.isEmpty()){
+                while (!queue.isEmpty()) {
                     int currentnode = queue.poll();
 
                     visited[currentnode] = true;
 
-                    for(int j=0; j<isConnected[0].length; j++){
-                        if(isConnected[currentnode][j]==1 && visited[j]==false){
+                    for (int j = 0; j < isConnected[0].length; j++) {
+                        if (isConnected[currentnode][j] == 1 && visited[j] == false) {
                             queue.offer(j);
                         }
                     }
