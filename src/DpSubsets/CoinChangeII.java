b0a07a941f673e5e1ec18023fba4134ead867168
Anudeep Billa <billa.anudeep143@gmail.com>
2024-10-12 17:39:20 -0400

    Coinchange II

---
+++ src/DpSubsets/CoinChangeII.java
@@ -0,0 +1,21 @@
+package DpSubsets;
+
+import java.util.Arrays;
+
+public class CoinChangeII {
+
+    public int change(int amount, int[] coins) {
+
+
+         int[] dp = new int[amount+1];
+         dp[0]=1;
+
+         for(int coin : coins){
+             for(int j= coin ; j<=amount; j++){
+                 dp[j]=dp[j]+dp[j-coin];
+             }
+         }
+
+         return dp[amount];
+    }
+}
