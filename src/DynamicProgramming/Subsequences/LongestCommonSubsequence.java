63c1ebd135d5f13b9fe4c008cf386c6d5f0a9cd2
Anudeep Billa <billa.anudeep143@gmail.com>
2024-10-16 20:26:44 -0400

    dp on subSequences, LCS, Tabulation

--- src/DynamicProgramming/Subsequences/LongestCommonSubsequence.java
+++ src/DynamicProgramming/Subsequences/LongestCommonSubsequence.java
@@ -40,4 +40,23 @@ public class LongestCommonSubsequence {
             return  dp[index1][index2] =  Math.max( text1Match, text2Match);
         }
     }
+
+    int Tabl(String text1, String text2, int n1, int n2){
+
+        int[][] dp = new int[n1+1][n2+1];
+
+         for(int i=1; i<=n1; i++){
+             for(int j=1; j<=n2; j++){
+
+                 if(text1.charAt(i-1) == text2.charAt(j-1)){
+                     dp[i][j] = 1 + dp[i-1][j-1];
+                 }
+                 else{
+                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                 }
+             }
+         }
+
+         return dp[n1][n2];
+    }
 }
