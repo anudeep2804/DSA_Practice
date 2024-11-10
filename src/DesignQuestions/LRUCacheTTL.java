f229fd2e903a5e6652dc6428cad96ad61cfbc254
Anudeep Billa <billa.anudeep143@gmail.com>
2024-11-09 21:47:59 -0500

    LRU Cache TTL

--- src/DesignQuestions/LRUCacheTTL.java
+++ src/DesignQuestions/LRUCacheTTL.java
@@ -4,17 +4,18 @@ import java.util.*;
 
 public class LRUCacheTTL {
 
-    LinkedList<Integer> list;  // Store keys instead of Cache objects
-    long TTL;
-    int capacity;  // Changed to lowercase following Java conventions
-    Map<Integer, Cache> map;
+    private LinkedList<Cache> list;  // Store Cache objects in usage order
+    private Map<Integer, Cache> map; // Store key -> Cache mapping
+    private long TTL;                // Time-to-live in milliseconds
+    private int capacity;            // Cache capacity
 
+    // Cache class to store key, value, and expiry time
     class Cache {
-        int key;    // Added key field
+        int key;
         int val;
         long expiryTime;
 
-        Cache(int key, int val) {  // Modified constructor to include key
+        Cache(int key, int val) {
             this.key = key;
             this.val = val;
             this.expiryTime = System.currentTimeMillis() + TTL;
@@ -28,49 +29,70 @@ public class LRUCacheTTL {
     public LRUCacheTTL(int capacity, long ttl) {
         this.capacity = capacity;
         this.TTL = ttl;
-        list = new LinkedList<>();
-        map = new HashMap<>();
+        this.list = new LinkedList<>();
+        this.map = new HashMap<>();
     }
 
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Cache currentCache = map.get(key);
-            if (currentCache.isExpired()) {
-                map.remove(key);
-                list.remove((Integer) key);
-                return -1;
-            } else {
-                list.remove((Integer) key);
-                list.addFirst(key);  // Store only the key in list
-                return currentCache.val;
-            }
+        if (!map.containsKey(key)) {
+            return -1; // Key not found
         }
-        return -1;
+
+        Cache cache = map.get(key);
+        if (cache.isExpired()) {
+            // Remove expired entry
+            removeCache(cache);
+            return -1;
+        }
+
+        // Move accessed cache to the front of the list
+        list.remove(cache);
+        list.addFirst(cache);
+        return cache.val;
     }
 
     public void put(int key, int value) {
-        // Remove expired entries first
-        Iterator<Integer> itr = list.iterator();
-        while (itr.hasNext()) {
-            int currentKey = itr.next();
-            Cache currentCache = map.get(currentKey);
-            if (currentCache.isExpired()) {
-                itr.remove();
-                map.remove(currentKey);
+        if (map.containsKey(key)) {
+            // Update existing entry and reset expiry
+            Cache cache = map.get(key);
+            cache.val = value;
+            cache.expiryTime = System.currentTimeMillis() + TTL;
+
+            // Move it to the front of the list
+            list.remove(cache);
+            list.addFirst(cache);
+        } else {
+            if (list.size() >= capacity) {
+                // Evict least recently used (last in the list)
+                Cache lruCache = list.removeLast();
+                map.remove(lruCache.key);
             }
+
+            // Add new cache entry
+            Cache newCache = new Cache(key, value);
+            list.addFirst(newCache);
+            map.put(key, newCache);
         }
 
-        if (map.containsKey(key)) {
-            list.remove((Integer) key);
-            list.addFirst(key);
-            map.put(key, new Cache(key, value));
-        } else {
-            if (map.size() >= capacity) {  // Changed == to >=
-                int lruKey = list.removeLast();  // Remove from the end, not the beginning
-                map.remove(lruKey);
+        // Remove expired items lazily if they exist
+        removeExpiredItems();
+    }
+
+    private void removeCache(Cache cache) {
+        list.remove(cache);  // Remove from list
+        map.remove(cache.key); // Remove from map
+    }
+
+    private void removeExpiredItems() {
+        Iterator<Cache> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Cache cache = iterator.next();
+            if (cache.isExpired()) {
+                iterator.remove();
+                map.remove(cache.key);
+            } else {
+                break; // Stop since the rest will be in order
             }
-            list.addFirst(key);
-            map.put(key, new Cache(key, value));
         }
     }
 }
